62ac64cab338a6bea192295183c5f82c67fc2c07
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-09-19 20:58:37 -0400

    Add HealthStatus enum (#3)
    * Add HealthStatus enum
    * Remove .keep files from src/main/java and src/test/java
    
    Fixes #1

---
+++ src/main/java/org/kiwiproject/metrics/health/HealthStatus.java
@@ -0,0 +1,246 @@
+package org.kiwiproject.metrics.health;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+import static java.util.stream.Collectors.toSet;
+import static org.kiwiproject.base.KiwiPreconditions.checkArgumentNotNull;
+import static org.kiwiproject.collect.KiwiMaps.isNullOrEmpty;
+
+import com.google.common.collect.Iterables;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.BooleanUtils;
+
+import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * This enum is used to indicate the health status/severity for both a service (i.e. multiple running instances)
+ * as well as the status of individual service instances.
+ */
+@Slf4j
+public enum HealthStatus {
+
+    /**
+     * Everything is OK and healthy.
+     * <p>
+     * This should only be used when health checks return {@code healthy=true}.
+     */
+    OK(1),
+
+    /**
+     * This can be used to provide additional information that may help to diagnose a problem, or simply to communicate
+     * some information that we might want to act upon.
+     * <p>
+     * This can be used whether health checks return {@code healthy=true} or {@code healthy=false}.
+     */
+    INFO(2),
+
+    /**
+     * Something is wrong or potentially wrong, or there is something concerning or that we need to take action on.
+     * <p>
+     * This can be used whether health checks return {@code healthy=true} or {@code healthy=false}.
+     */
+    WARN(3),
+
+    /**
+     * There is an actual problem that needs to be addressed and corrected quickly for the service to function properly.
+     * <p>
+     * This should only be used when health checks return {@code healthy=false}.
+     */
+    CRITICAL(4),
+
+    /**
+     * This level is reserved for the case when all instances of a service are down, and thus the system cannot
+     * perform its services and functions. It needs to be corrected immediately.
+     * <p>
+     * This should only be used when there are no service instances.
+     */
+    FATAL(5);
+
+    private static final String SEVERITY_DETAILS_KEY = "severity";
+
+    /**
+     * Internal value used to compare severity (we do NOT want to rely on the ordinal of the enum constants).
+     */
+    private final int value;
+
+    HealthStatus(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Given a map containing the results of all the health checks in a service instance (e.g. the JSON that is
+     * returned by calling the {@code healthcheck} endpoint of an instance), determine the appropriate health status
+     * by checking both the {@code healthy} flag (true or false) as well as the {@code severity} if present.
+     *
+     * @implNote Expects a map of maps structured like: {@code string ->(string -> object)}. The map's keys
+     * are the names of the individual checks (e.g. database, serverErrors, rottenTomato, etc.) while the values
+     * are maps containing the health check result, which at a minimum should contain a boolean {@code healthy} and
+     * can contain a {@code severity} whose values should be the exact names of this enum as a string, e.g. "INFO".
+     */
+    public static HealthStatus from(Map<String, Object> healthDetails) {
+        if (isNullOrEmpty(healthDetails)) {
+            return CRITICAL;
+        }
+
+        var healthStatuses = healthDetails.values()
+                .stream()
+                .filter(value -> value instanceof Map)
+                .map(Map.class::cast)
+                .map(HealthStatus::determineOverallStatus)
+                .collect(toSet());
+
+        if (healthStatuses.isEmpty()) {
+            return CRITICAL;
+        }
+
+        if (healthStatuses.size() == 1) {
+            return Iterables.getOnlyElement(healthStatuses);
+        }
+
+        return highestSeverity(healthStatuses);
+    }
+
+    private static HealthStatus determineOverallStatus(Map<String, Object> map) {
+        var healthy = getHealthyValue(map);
+        var severity = getHealthStatusOrNull(map);
+        return determineOverallStatus(healthy, severity);
+    }
+
+    // Assumes the map contains a "healthy" key with boolean value, otherwise returns false.
+    private static boolean getHealthyValue(Map<String, Object> map) {
+        try {
+            var value = map.getOrDefault("healthy", Boolean.FALSE).toString();
+            return Boolean.parseBoolean(value);
+        } catch (Exception e) {
+            LOG.warn("Something gave us a 'healthy' value that threw an exception on toString()");
+            return false;
+        }
+    }
+
+    // Suppress "Exception handlers should preserve the original exceptions" - we know the cause in this case is that
+    // the severity value is null or not a valid enum constant, so the stack trace provides no additional help
+    @SuppressWarnings("java:S1166")
+    private static HealthStatus getHealthStatusOrNull(Map<String, Object> map) {
+        if (!map.containsKey(SEVERITY_DETAILS_KEY)) {
+            return null;
+        }
+
+        Object severityObj = map.get(SEVERITY_DETAILS_KEY);
+        String severity = severityOrNull(severityObj);
+
+        try {
+            return Optional.ofNullable(severity)
+                    .map(HealthStatus::valueOf)
+                    .orElse(HealthStatus.WARN);
+        } catch (Exception e) {
+            LOG.error("Something gave us an invalid severity: {} (returning WARN). Health map: {}", severity, map);
+            return WARN;
+        }
+    }
+
+    private static String severityOrNull(Object severityObj) {
+        if (severityObj instanceof String) {
+            return (String) severityObj;
+        }
+
+        LOG.warn("Something gave us a severity that was not a String: {}", severityObj);
+        return null;
+    }
+
+    private static HealthStatus determineOverallStatus(boolean healthy, @Nullable HealthStatus severity) {
+        if (isInvalidCombination(healthy, severity)) {
+            LOG.warn("Detected invalid (healthy, severity) combination: ({}, {})", healthy, severity);
+            return HealthStatus.max(WARN, Optional.ofNullable(severity).orElse(WARN));
+        }
+
+        if (isNull(severity)) {
+            return HealthStatus.from(healthy);
+        }
+
+        if (healthy) {
+            return HealthStatus.max(OK, severity);
+        }
+
+        return severity;
+    }
+
+    /**
+     * Return the default severity for the given healthy value.
+     *
+     * @param healthy true if healthy, false otherwise
+     * @return the default HealthStatus
+     */
+    public static HealthStatus defaultSeverityForValue(boolean healthy) {
+        return healthy ? OK : WARN;
+    }
+
+    /**
+     * Is the given (healthy, severity) combination valid?
+     *
+     * @param healthy  true if healthy, false otherwise
+     * @param severity the severity, if null the return value will always be true
+     * @return true if the given (healthy, severity) combination is valid, otherwise false
+     */
+    public static boolean isValidCombination(boolean healthy, @Nullable HealthStatus severity) {
+        return !isInvalidCombination(healthy, severity);
+    }
+
+    /**
+     * Is the given (healthy, severity) combination invalid?
+     *
+     * @param healthy  true if healthy, false otherwise
+     * @param severity the severity, if null the return value will always be false (i.e. it's a valid combination)
+     * @return true if the given (healthy, severity) combination is NOT valid, otherwise false
+     */
+    public static boolean isInvalidCombination(boolean healthy, @Nullable HealthStatus severity) {
+        if (isNull(severity)) {
+            return false;
+        }
+
+        var healthyWithInvalidSeverity = healthy && (severity == CRITICAL || severity == FATAL);
+        var unhealthyWithInvalidSeverity = !healthy && severity == OK;
+
+        return healthyWithInvalidSeverity || unhealthyWithInvalidSeverity;
+    }
+
+    /**
+     * Return {@link #OK} if {@code value} is {@code true}; {@link #WARN} otherwise (including if {@code null}).
+     */
+    public static HealthStatus from(Boolean value) {
+        return BooleanUtils.toBoolean(value) ? OK : WARN;
+    }
+
+    /**
+     * Return the higher of the two given status values.
+     *
+     * @param status1 the first status
+     * @param status2 the second status
+     * @throws IllegalArgumentException if either argument is null
+     */
+    public static HealthStatus max(HealthStatus status1, HealthStatus status2) {
+        checkArgumentNotNull(status1);
+        checkArgumentNotNull(status2);
+
+        return compare(status1, status2) > 0 ? status1 : status2;
+    }
+
+    /**
+     * Return the highest severity in the (non-null, non-empty) collection of status values.
+     *
+     * @throws IllegalArgumentException if the status list is null or empty
+     */
+    public static HealthStatus highestSeverity(Collection<HealthStatus> statuses) {
+        checkArgument(nonNull(statuses) && !statuses.isEmpty(), "statuses cannot be empty or null");
+
+        return Collections.max(statuses, HealthStatus::compare);
+    }
+
+    private static int compare(HealthStatus status1, HealthStatus status2) {
+        return Integer.compare(status1.value, status2.value);
+    }
+}
---
+++ src/test/java/org/kiwiproject/metrics/health/HealthStatusTest.java
@@ -0,0 +1,398 @@
+package org.kiwiproject.metrics.health;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.kiwiproject.base.KiwiStrings;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@DisplayName("HealthStatus")
+class HealthStatusTest {
+
+    @Nested
+    class HealthStatusFrom {
+
+        @Nested
+        class EdgeCases {
+
+            @Test
+            void
+            shouldIgnore_MapThatContainsNonMapValue_WhenOtherValidMapsExist() {
+                Map<String, Object> healthDetails = Map.of(
+                        "database", Map.of("healthy", true),
+                        "badThing", "this is NOT a map and will be ignored"
+                );
+                assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.OK);
+            }
+
+            @Test
+            void shouldBe_OK_WhenGiven_MapWithStringHealthyValue() {
+                Map<String, Object> healthDetails = Map.of(
+                        "database", Map.of("healthy", "TRUE", "message", "Connection refused")
+                );
+
+                assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.OK);
+            }
+
+            @Test
+            void shouldBe_WARN_WhenGiven_MapWithHealthyValue_WhoseToStringThrowsAnException() {
+                Map<String, Object> healthDetails = Map.of(
+                        "database", Map.of("healthy", new EvilToString())
+                );
+
+                assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.WARN);
+            }
+        }
+    }
+
+    private static class EvilToString {
+        @Override
+        public String toString() {
+            throw new RuntimeException("I am evil");
+        }
+    }
+
+    @Nested
+    class InvalidArgument {
+
+        @Test
+        void shouldBe_CRITICAL_WhenGiven_ObjectThatIsNotMap() {
+            Map<String, Object> healthDetails = Map.of(
+                    "database", "this is definitely NOT a map"
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.CRITICAL);
+        }
+
+        @Test
+        void shouldBe_CRITICAL_WhenGiven_NullMap() {
+            assertThat(HealthStatus.from((Map<String, Object>) null)).isEqualTo(HealthStatus.CRITICAL);
+        }
+
+        @Test
+        void shouldBe_CRITICAL_WhenGiven_EmptyMap() {
+            assertThat(HealthStatus.from(Map.of())).isEqualTo(HealthStatus.CRITICAL);
+        }
+
+        @Test
+        void shouldBe_WARN_WhenGiven_MapWithNoHealthyKey() {
+            Map<String, Object> healthDetails = Map.of(
+                    "database", Map.of("message", "Connection refused")
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.WARN);
+        }
+
+        @Test
+        void shouldBe_WARN_WhenGiven_MapWithObjectHealthyValue() {
+            Map<String, Object> healthDetails = Map.of(
+                    "database", Map.of("healthy", new Object(), "message", "Connection refused")
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.WARN);
+        }
+
+        @Test
+        void shouldBe_WARN_WhenGiven_AnInvalidSeverityLevel() {
+            Map<String, Object> healthDetails = Map.of(
+                    "database", Map.of("healthy", false, "severity", "FOO")
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.WARN);
+        }
+
+        @Test
+        void shouldBe_WARN_WhenGiven_MapWithSeverityLevel() {
+            Map<String, Object> healthDetails = Map.of(
+                    "database", Map.of(
+                            "healthy", false,
+                            "severity", Map.of("level", "WARN")
+                    )
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.WARN);
+        }
+    }
+
+    @Nested
+    class WhenSingleEntryMap {
+
+        @Test
+        void shouldBe_OK_WhenHealthy() {
+            Map<String, Object> healthDetails = Map.of(
+                    "database", Map.of("healthy", true)
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.OK);
+        }
+
+        @Test
+        void shouldBe_INFO_WhenHealthy_AndHas_INFOSeverity() {
+            Map<String, Object> healthDetails = Map.of(
+                    "something", Map.of("healthy", true, "severity", "INFO"));
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.INFO);
+        }
+
+        @Test
+        void shouldBe_WARN_WhenNotHealthy_AndHasNoSeverity() {
+            Map<String, Object> healthDetails = Map.of(
+                    "database", Map.of("healthy", false, "message", "Connection refused")
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.WARN);
+        }
+
+        @Test
+        void shouldBe_WARN_WhenNotHealthy_AndHasWARNSeverity() {
+            Map<String, Object> healthDetails = Map.of(
+                    "aThing", Map.of("healthy", false, "severity", "WARN")
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.WARN);
+        }
+
+        @Test
+        void shouldBe_CRITICAL_WhenNotHealthy_AndHasCRITICALSeverity() {
+            Map<String, Object> healthDetails = Map.of(
+                    "aThing", Map.of("healthy", false, "severity", "CRITICAL"));
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.CRITICAL);
+        }
+    }
+
+    @Nested
+    class WhenMultiEntryMap {
+
+        @Test
+        void shouldBe_OK_WhenAllHealthy() {
+            Map<String, Object> healthDetails = Map.of(
+                    "deadlocks", Map.of("healthy", true),
+                    "database", Map.of("healthy", true),
+                    "rottenTomato", Map.of("healthy", true, "message", "No errors in last 15 minutes")
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.OK);
+        }
+
+        @Test
+        void shouldBe_INFO_WhenAllHealthy_WithINFOSeverity() {
+            Map<String, Object> healthDetails = Map.of(
+                    "deadlocks", Map.of("healthy", true),
+                    "database", Map.of("healthy", true),
+                    "rottenTomato", Map.of("healthy", true, "message", "No errors in last 15 minutes"),
+                    "somethingElse", Map.of("healthy", true, "severity", "INFO")
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.INFO);
+        }
+
+        @Test
+        void shouldBe_WARN_WhenOneNotHealthy_AndNoSeverity() {
+            Map<String, Object> healthDetails = Map.of(
+                    "deadlocks", Map.of("healthy", true),
+                    "database", Map.of("healthy", false, "message", "Connection refused"),
+                    "rottenTomato", Map.of("healthy", true, "message", "No errors in last 15 minutes")
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.WARN);
+        }
+
+        @Test
+        void shouldBe_WARN_WhenOneNotHealthy_AndHasWARNSeverity() {
+            Map<String, Object> healthDetails = Map.of(
+                    "deadlocks", Map.of("healthy", true),
+                    "database", Map.of("healthy", true),
+                    "rottenTomato", Map.of("healthy", true, "message", "No errors in last 15 minutes"),
+                    "notThatImportant", Map.of("healthy", false, "severity", "WARN")
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.WARN);
+        }
+
+        @Test
+        void shouldBe_CRITICAL_WhenOneNotHealthy_AndHasCRITICALSeverity() {
+            Map<String, Object> healthDetails = Map.of(
+                    "deadlocks", Map.of("healthy", true),
+                    "database", Map.of("healthy", true),
+                    "rottenTomato", Map.of("healthy", true, "message", "No errors in last 15 minutes"),
+                    "extremelyImportant", Map.of("healthy", false, "severity", "CRITICAL"));
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.CRITICAL);
+        }
+
+        @Test
+        void shouldBe_WARN_WhenAllNotHealthy_ButNoneHaveSeverity() {
+            Map<String, Object> healthDetails = Map.of(
+                    "deadlocks", Map.of("healthy", false),
+                    "database", Map.of("healthy", false, "message", "Connection refused"),
+                    "rottenTomato", Map.of("healthy", false, "message", "27 errors in last 15 minutes")
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.WARN);
+        }
+
+        @Test
+        void shouldBe_WARN_WhenAllNotHealthy_WithHighestSeverityOfWARN() {
+            Map<String, Object> healthDetails = Map.of(
+                    "deadlocks", Map.of("healthy", false),
+                    "database", Map.of("healthy", false, "message", "Connection refused"),
+                    "rottenTomato", Map.of("healthy", false, "message", "27 errors in last 15 minutes"),
+                    "somewhatImportant", Map.of("healthy", false, "severity", "WARN"));
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.WARN);
+        }
+
+        @Test
+        void shouldBe_CRITICAL_WhenAllNotHealthy_WithHighSeverityOfCRITICAL() {
+            Map<String, Object> healthDetails = Map.of(
+                    "deadlocks", Map.of("healthy", false),
+                    "database", Map.of("healthy", false, "message", "Connection refused"),
+                    "rottenTomato", Map.of("healthy", false, "message", "27 errors in last 15 minutes"),
+                    "somewhatImportant", Map.of("healthy", false, "severity", "CRITICAL")
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(HealthStatus.CRITICAL);
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "true,CRITICAL,CRITICAL",
+                "true,FATAL,FATAL",
+                "false,OK,WARN",
+        })
+        void shouldReturnExpectedHealthStatusFrom_WhenInvalidHealthyAndSeverityCombination(boolean healthy,
+                                                                                           String severity,
+                                                                                           HealthStatus expectedStatus) {
+            Map<String, Object> healthDetails = Map.of(
+                    "deadlocks", Map.of("healthy", true),
+                    "something", Map.of("healthy", healthy, "severity", severity)
+            );
+
+            assertThat(HealthStatus.from(healthDetails)).isEqualTo(expectedStatus);
+        }
+    }
+
+    @Nested
+    class DefaultSeverityForValue {
+
+        @Test
+        void shouldBe_OK_ForHealthy() {
+            assertThat(HealthStatus.defaultSeverityForValue(true)).isEqualTo(HealthStatus.OK);
+        }
+
+        @Test
+        void shouldBe_WARN_ForUnhealthy() {
+            assertThat(HealthStatus.defaultSeverityForValue(false)).isEqualTo(HealthStatus.WARN);
+        }
+    }
+
+    @Nested
+    class IsValidCombination {
+
+        @ParameterizedTest
+        @ValueSource(booleans = {true, false})
+        void shouldAllowNullSeverity(boolean healthy) {
+            assertThat(HealthStatus.isValidCombination(healthy, null)).isTrue();
+            assertThat(HealthStatus.isInvalidCombination(healthy, null)).isFalse();
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "true, OK",
+                "true, INFO",
+                "true, WARN",
+                "false, INFO",
+                "false, WARN",
+                "false, CRITICAL",
+                "false, FATAL",
+        })
+        void shouldBeTrue_WhenValid(boolean healthy, HealthStatus severity) {
+            assertThat(HealthStatus.isValidCombination(healthy, severity)).isTrue();
+            assertThat(HealthStatus.isInvalidCombination(healthy, severity)).isFalse();
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "false, OK",
+                "true, CRITICAL",
+                "true, FATAL"})
+        void shouldBeFalse_WhenNotValid(boolean healthy, HealthStatus severity) {
+            assertThat(HealthStatus.isInvalidCombination(healthy, severity)).isTrue();
+            assertThat(HealthStatus.isValidCombination(healthy, severity)).isFalse();
+        }
+    }
+
+    @Nested
+    class HighestSeverity {
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void shouldThrowException_WhenGivenNullStatusList(List<HealthStatus> statuses) {
+            assertThatThrownBy(() -> HealthStatus.highestSeverity(statuses))
+                    .isExactlyInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                " 'OK, OK, OK', OK",
+                " 'OK, INFO, OK', INFO",
+                " 'OK, OK, WARN, INFO', WARN",
+                " 'OK, OK, WARN, INFO, OK, CRITICAL, OK', CRITICAL",
+                " 'OK, FATAL, WARN, INFO, OK, CRITICAL, OK', FATAL"
+        })
+        void shouldReturn_HighestSeverity_FromCollection(String statusCsv, HealthStatus expectedHighest) {
+            var statuses = Arrays.stream(statusCsv.split(","))
+                    .map(String::trim)
+                    .map(HealthStatus::valueOf)
+                    .collect(toUnmodifiableList());
+
+            assertThat(HealthStatus.highestSeverity(statuses)).isEqualTo(expectedHighest);
+        }
+    }
+
+    @Nested
+    class Max {
+        @ParameterizedTest
+        @CsvSource({
+                " '' , '' ",
+                " '', OK",
+                " OK , '' "
+        })
+        void shouldNotAllowNullArguments(String arg1, String arg2) {
+            var status1 = Optional.ofNullable(KiwiStrings.blankToNull(arg1)).map(HealthStatus::valueOf).orElse(null);
+            var status2 = Optional.ofNullable(KiwiStrings.blankToNull(arg2)).map(HealthStatus::valueOf).orElse(null);
+
+            assertThatThrownBy(() -> HealthStatus.max(status1, status2))
+                    .isExactlyInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "OK,OK,OK",
+                "OK,INFO,INFO",
+                "INFO,INFO,INFO",
+                "INFO,WARN,WARN",
+                "WARN,WARN,WARN",
+                "WARN,CRITICAL,CRITICAL",
+                "CRITICAL,CRITICAL,CRITICAL",
+                "CRITICAL,FATAL,FATAL"
+        })
+        void shouldReturnHigherStatus(HealthStatus status1, HealthStatus status2, HealthStatus expectedMax) {
+            assertThat(HealthStatus.max(status1, status2)).isEqualTo(expectedMax);
+            assertThat(HealthStatus.max(status2, status1)).isEqualTo(expectedMax);
+        }
+    }
+}
